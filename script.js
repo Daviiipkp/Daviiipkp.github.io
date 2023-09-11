8a0da76a291c9c518af3e554464df013df278339
Daviiipkp <85971755+Daviiipkp@users.noreply.github.com>
2023-09-11 03:16:55 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function mudarTexto() {
-    var elemento = document.querySelector('.title-text');
-    elemento.textContent = "aula do meu professor favorito :P";
-  }
-
-  function mudarTexto2() {
-    var elemento = document.querySelector('.title-text');
-    elemento.textContent = "(mentira, prefiro harly)";
-  }
